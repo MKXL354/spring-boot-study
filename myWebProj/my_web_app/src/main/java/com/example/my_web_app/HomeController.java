97a602801437ae4cea854c5a769672918ed49534
MKXL354 <mahdykamali354@yahoo.com>
2024-04-18 10:25:07 +0330

    feat: accepting client data

--- myWebProj/my_web_app/src/main/java/com/example/my_web_app/HomeController.java
+++ myWebProj/my_web_app/src/main/java/com/example/my_web_app/HomeController.java
@@ -3,16 +3,17 @@ package com.example.my_web_app;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+
 @Controller
 public class HomeController {
     @RequestMapping("home")
-    public String home() {
-        System.out.println("home accessed");
+    public String home(HttpServletRequest req) {
+        HttpSession session = req.getSession();
+        String name = req.getParameter("name");
+        session.setAttribute("name", name);
+        System.out.println(name);
         return "home";
-    }
-
-    @RequestMapping("test")
-    public void test() {
-        System.out.println("test accessed");
     }
 }
--- myWebProj/my_web_app/src/main/webapp/pages/home.jsp
+++ myWebProj/my_web_app/src/main/webapp/pages/home.jsp
@@ -5,6 +5,6 @@
     <title>JSP Page</title>
 </head>
 <body>
-    <h1>Hello, JSP!</h1>
+    <h1>Hello, ${name}!</h1>
 </body>
 </html>
