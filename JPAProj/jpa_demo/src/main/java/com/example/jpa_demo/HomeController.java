05c3d006793cc06f5fdf354a6afa3d6b59da1bda
MKXL354 <mahdykamali354@yahoo.com>
2024-04-20 21:22:11 +0330

    feat: @RestController

--- JPAProj/jpa_demo/src/main/java/com/example/jpa_demo/HomeController.java
+++ JPAProj/jpa_demo/src/main/java/com/example/jpa_demo/HomeController.java
@@ -4,28 +4,33 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.example.jpa_demo.dao.AlienRepo;
 import com.example.jpa_demo.model.Alien;
 
-@Controller
+@RestController
 public class HomeController {
 
     @Autowired
     AlienRepo repo;
 
-    @RequestMapping(path = "/aliens", produces = "application/xml")
-    @ResponseBody
-    public List<Alien> getlAiens() {
+    @GetMapping("/aliens")
+    public List<Alien> getAliens() {
         return repo.findAll();
     }
 
-    @RequestMapping(path = "/alien/{id}", produces = "application/xml")
-    @ResponseBody
+    @GetMapping("/alien/{id}")
     public Optional<Alien> getAlien(@PathVariable("id") int id) {
         return repo.findById(id);
+    }
+
+    @PostMapping("/alien")
+    public Alien postAlien(Alien alien) {
+        repo.save(alien);
+        return alien;
     }
 }
