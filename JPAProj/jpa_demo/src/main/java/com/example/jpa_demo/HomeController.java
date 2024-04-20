edbf6e36c5658ad606231c8dfc1901be997183fd
MKXL354 <mahdykamali354@yahoo.com>
2024-04-20 22:04:04 +0330

    feat: GET/PUT/POST/DELETE using postman

--- JPAProj/jpa_demo/src/main/java/com/example/jpa_demo/HomeController.java
+++ JPAProj/jpa_demo/src/main/java/com/example/jpa_demo/HomeController.java
@@ -4,9 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.jpa_demo.dao.AlienRepo;
@@ -29,8 +33,28 @@ public class HomeController {
     }
 
     @PostMapping("/alien")
-    public Alien postAlien(Alien alien) {
+    public Alien postAlien(@RequestBody Alien alien) {
         repo.save(alien);
         return alien;
     }
+    // You can send form-data here
+
+    @DeleteMapping("/alien/{id}")
+    public String deleteAlien(@PathVariable("id") int id) {
+        Alien a = repo.findById(id).orElse(new Alien());
+        try {
+            repo.delete(a);
+            return id + " deleted";
+        } catch (EmptyResultDataAccessException e) {
+            return "not found";
+        }
+    }
+    // REST delete has no body
+
+    @PutMapping("/alien")
+    public Alien putAlien(@RequestBody Alien alien) {
+        repo.save(alien);
+        return alien;
+    }
+    // You should send raw data here
 }
